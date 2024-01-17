fb9ae980817c9274175b832c5d29adf852b65771
SimonBkr <simonboker95@gmail.com>
2024-01-17 17:34:04 +0100

    - reworked player sprite+

--- Characters/MainChar/MainChar.gd
+++ Characters/MainChar/MainChar.gd
@@ -24,11 +24,18 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	if direction:
+		if direction.x > 0:
+			$AnimatedSprite2D.flip_h = false
+		elif direction.x < 0:
+			$AnimatedSprite2D.flip_h = true
+			
 		velocity.x = direction.x * SPEED
 		velocity.y = direction.y * SPEED
+		$AnimatedSprite2D.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		$AnimatedSprite2D.play("idle")
 
 	if move_and_slide():
 		if oldCollider != get_slide_collision(0).get_collider():
