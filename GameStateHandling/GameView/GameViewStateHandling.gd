9533f05b774a380ec1602944f5f2c6e6dd2efe48
CptBalusen <64691127+CptBalusen@users.noreply.github.com>
2024-01-19 22:03:00 +0100

    GameView über GameSceneIDS
    -SceneIDS in an enum
    -GameView in a new folder
    -gitignore now should also ignore any file with *.tmp

--- GameStateHandling/GameView/GameViewStateHandling.gd
+++ GameStateHandling/GameView/GameViewStateHandling.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var GameScenes = $Scripts/GameSceneIDS
 var childInstance
 var childSceneID
 var sceneArray = [null, null,null,null,null,null,null]
@@ -7,42 +8,41 @@ var savedPackagedSceneArray = [null, null,null,null,null,null,null]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if(get_child_count() > 0):
-		print(get_child_count())
-		return
 	var childScene = load("res://UserInterfaces/MainMenue/cs_main_menue.tscn")
-	savedPackagedSceneArray[0] = childScene
+	savedPackagedSceneArray[GameScenes.IDs.MainMenu] = childScene
 	childInstance = childScene.instantiate()
 	add_child(childInstance)
-	get_child(0).scene_changer.connect(scene_change)
+	get_child(1).scene_changer.connect(scene_change)
 	childSceneID = 0
+	sceneArray[0] = childInstance
 	_init_sceneArray()
 	pass # Replace with function body.
 	
 func _init_sceneArray():
-	sceneArray[0] = childInstance
 	var sceneToAdd
 	
+	#0 also Main Menu wird in _ready hinzugefügt
+	
 	sceneToAdd = load("res://UserInterfaces/SettingsMenue/cs_settings.tscn")
-	savedPackagedSceneArray[1] = sceneToAdd
+	savedPackagedSceneArray[GameScenes.IDs.Settings] = sceneToAdd
 	if(sceneToAdd == null):
 		print("ERROR IN SCENE ARRAY LOADING SETTINGS")
 
 	
-	sceneToAdd = load("res://CityView.tscn")
-	savedPackagedSceneArray[2] = sceneToAdd
+	sceneToAdd = load("res://Level/City1/CityView.tscn")
+	savedPackagedSceneArray[GameScenes.IDs.CityView] = sceneToAdd
 	if(sceneToAdd == null):
 		print("ERROR IN SCENE ARRAY LOADING CITYVIEW")
 
 	
 	sceneToAdd = load("res://Buildings/House1/House1Interior.tscn")
-	savedPackagedSceneArray[3] = sceneToAdd
+	savedPackagedSceneArray[GameScenes.IDs.House1Interior] = sceneToAdd
 	if(sceneToAdd == null):
 		print("ERROR IN SCENE ARRAY LOADING HOUSE1 INTERIOR")
  
 	
 	sceneToAdd = load("res://Buildings/House1/House2Interior.tscn")
-	savedPackagedSceneArray[4] = sceneToAdd
+	savedPackagedSceneArray[GameScenes.IDs.House2Interior] = sceneToAdd
 	if(sceneToAdd == null):
 		print("ERROR IN SCENE ARRAY LOADING HOUSE2 INTERIOR")
 
@@ -62,16 +62,20 @@ func _check_reset(id):
 
 func scene_change(sceneID):
 	print("ChildScene before Change ", childSceneID)
+	var signalConnectionNeeded = false
 	if(sceneArray[sceneID] == null):
 		sceneArray[sceneID] = savedPackagedSceneArray[sceneID].instantiate()
-	if(sceneArray[sceneID] != null):
-		if(_check_reset(childSceneID)):
-			get_child(0).queue_free()
-			sceneArray[childSceneID] = null
-		remove_child(get_child(0))
-		sceneArray[sceneID].request_ready()
-		add_child(sceneArray[sceneID])
-		get_children()[0].scene_changer.connect(scene_change)
+		signalConnectionNeeded = true
+	if(_check_reset(childSceneID)):
+		get_child(1).queue_free()
+		sceneArray[childSceneID] = null
+	remove_child(get_child(1))
+	sceneArray[sceneID].request_ready()
+	add_child(sceneArray[sceneID])
+	
+	if(signalConnectionNeeded):
+		get_children()[1].scene_changer.connect(scene_change)
+		
 	childSceneID = sceneID
 	print("ChildScene after Change ", childSceneID)
 	pass
